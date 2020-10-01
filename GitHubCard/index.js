347d4150c9fcbc551c5ea459239919e0d07a946c
Wesley-Ryan <69822796+Wesley-Ryan@users.noreply.github.com>
2020-10-01 14:37:46 -0700

    Revert "Wesley White"

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,7 +3,6 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-import axios from "axios";
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -29,100 +28,7 @@ import axios from "axios";
     user, and adding that card to the DOM.
 */
 
-//add input to change user
-
-const btn = document.createElement("div");
-btn.classList.add("button");
-btn.textContent = "Search";
-
-const inputBox = document.createElement("input");
-
-inputBox.setAttribute("type", "text");
-inputBox.classList.add("input");
-
-const entryPoint = document.querySelector(".cards");
-const searchContainer = document.createElement("div");
-searchContainer.classList.add("search-container");
-searchContainer.appendChild(inputBox);
-searchContainer.appendChild(btn);
-entryPoint.append(searchContainer);
-
-const getInput = () => {
-  return inputBox.value;
-};
-
-btn.addEventListener("click", () => {
-  userName = getInput();
-  getInfo();
-});
-
-let userName = "Wesley-Ryan";
-
-const getInfo = () => {
-  axios
-    .get(`https://api.github.com/users/${userName}`)
-    .then((response) => {
-      let user = response.data;
-      let card = cardCreator(user);
-      entryPoint.appendChild(card);
-    })
-    .catch((err) => {
-      console.log("Error:", err);
-    });
-};
-
 const followersArray = [];
-
-const cardCreator = (user) => {
-  console.log("THIS IS THE USER IN MY CARD CREATOR:", user);
-  const card = document.createElement("div");
-  const cardImg = document.createElement("img");
-  const cardInfo = document.createElement("div");
-  const cardTitle = document.createElement("h3");
-  const cardSubtitle = document.createElement("p");
-  const cardLocation = document.createElement("p");
-  const cardProfile = document.createElement("p");
-  const cardLink = document.createElement("a");
-  const cardLinkText = document.createTextNode(user.html_url);
-
-  const cardFollower = document.createElement("p");
-  const cardFollowing = document.createElement("p");
-
-  //position elements
-  card.appendChild(cardImg);
-  card.appendChild(cardInfo);
-  cardInfo.appendChild(cardTitle);
-  cardInfo.appendChild(cardSubtitle);
-  cardInfo.appendChild(cardLocation);
-  cardInfo.appendChild(cardProfile);
-  cardLocation.appendChild(cardLink);
-  cardInfo.appendChild(cardLink);
-  cardInfo.appendChild(cardFollower);
-  cardInfo.appendChild(cardFollowing);
-
-  cardLink.appendChild(cardLinkText);
-
-  //add content to elements
-  card.classList.add("card");
-  cardInfo.classList.add("card-info");
-  cardTitle.classList.add("name");
-  cardSubtitle.classList.add("username");
-  cardImg.src = user.avatar_url;
-  cardTitle.textContent = user.name;
-  cardSubtitle.textContent = user.login;
-  cardLocation.textContent = `Location:${user.location}`;
-  cardProfile.textContent = `Profile:`;
-  cardLink.setAttribute("href", user.html_url);
-
-  cardFollower.textContent = `Followers:${user.followers}`;
-  cardFollowing.textContent = `Following: ${user.following}`;
-
-  //do some stuff
-
-  return card;
-};
-
-getInfo();
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
